0261bbf67911fe44e23f707e207b574ae2e6111c
Christopher Edwards <cedwards036@gmail.com>
2019-04-05 14:10:59 -0400

    Add basic directory structure

---
+++ setup.py
@@ -0,0 +1,28 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="autohandshake",
+    version="1.0.0",
+    description="A library for automating tasks on the Handshake career services platform",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cedwards036/autohandshake",
+    author="Christopher Edwards",
+    author_email="cedwards036@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+    ],
+    packages=["autohandshake"],
+    include_package_data=True,
+)
