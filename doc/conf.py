5c9f42c38e23186b6d9e352e7b3e00fffe244ff7
Christopher Edwards <cedwards036@gmail.com>
2019-04-22 16:24:35 -0400

    Increment version number after minor documentation changes

--- doc/conf.py
+++ doc/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, Christopher Edwards'
 author = 'Christopher Edwards'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.4'
+release = '1.0.5'
 
 # -- General configuration ---------------------------------------------------
 
