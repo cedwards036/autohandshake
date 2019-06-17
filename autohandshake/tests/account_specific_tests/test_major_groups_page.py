e17ba9aef94a137c20970e99811a75e46813224c
Christopher Edwards <cedwards036@gmail.com>
2019-06-17 10:49:03 -0400

    Fix out of date major group test values

--- autohandshake/tests/account_specific_tests/test_major_groups_page.py
+++ autohandshake/tests/account_specific_tests/test_major_groups_page.py
@@ -9,12 +9,12 @@ class TestMajorSettingsPage(unittest.TestCase):
         with TestSession() as browser:
             major_settings_page = MajorSettingsPage(browser)
             mapping = major_settings_page.get_major_mapping()
-            self.assertEqual('AD: Piano', mapping[3]['major'])
-            self.assertEqual(['Music & Music Education', 'Visual & Performing Arts'], sorted(mapping[3]['groups']))
+            self.assertEqual('AD: Piano', mapping[4]['major'])
+            self.assertEqual(['Music & Music Education', 'Visual & Performing Arts'], sorted(mapping[4]['groups']))
 
-            self.assertEqual('Applied Physics', mapping[12]['major'])
-            self.assertEqual(['Aerospace Engineering', 'General Engineering', 'Physics'], sorted(mapping[12]['groups']))
+            self.assertEqual('Applied Physics', mapping[13]['major'])
+            self.assertEqual(['Aerospace Engineering', 'General Engineering', 'Physics'], sorted(mapping[13]['groups']))
 
-            self.assertEqual('Bachelors: Film & Media Studies', mapping[30]['major'])
+            self.assertEqual('Bachelors: Film & Media Studies', mapping[33]['major'])
             self.assertEqual(['Documentary/Film', 'Public Relations', 'Radio, Television, Media'],
-                             sorted(mapping[30]['groups']))
+                             sorted(mapping[33]['groups']))
